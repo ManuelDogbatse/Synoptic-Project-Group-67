fbe62429fadbe42beb4da20550424bc70b6823c9
Rbab96 <98762928+Rbab96@users.noreply.github.com>
2022-06-09 05:05:46 +0100

    Extracted files

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 // Initialization
-const express = require('express')
-const app = express()
-var bodyParser = require('body-parser'); 
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser');
 
-app.use(bodyParser.json());                         //Using body parser to handle form data
-app.use(bodyParser.urlencoded({extended:false}))    //Avoids data from becoming URL encoded
-app.use(express.static('public')); 
+app.use(bodyParser.json());                          //Using body parser to handle form data
+app.use(bodyParser.urlencoded({extended:false}));    //Avoids data from becoming URL encoded
+app.use(express.static('public'));
+
+const me =
+{
+    name: "Manuel",
+    age: 20
+};
 
 // Get request for index
 app.get('/', (req, res) => {
-    res.render('index.ejs',)
-})
+    res.render('index.ejs');
+});
+
+// Get request for form
+app.get('/form', (req, res) => {
+    res.render('form.ejs');
+});
+
 
 // Listen on port 3000
-app.listen(3000)
+app.listen(3000);
