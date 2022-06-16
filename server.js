b5990e8e593ae2edf52ffe2e471e8b1b6f2518c1
ManuelDogbatse <85999078+ManuelDogbatse@users.noreply.github.com>
2022-06-16 01:37:49 +0100

    Added default town values

--- server.js
+++ server.js
@@ -20,7 +20,150 @@ if (formFileDataJS.length == 0)
 {
     formFileDataJS.push(
     {
-        defaultValues: [],
+        defaultValues: [
+            {
+                townName: "Bamaga",
+                season: "Wet",
+                temperature: "14",
+                rainfall: "419",
+                ph: "4.5",
+                produce: [
+                    "Crop1",
+                    "Crop2"
+                ]
+            },
+            {
+                townName: "Mapoon",
+                season: "Dry",
+                temperature: "17",
+                rainfall: "427",
+                ph: "7",
+                produce: [
+                    "Crop3",
+                    "Crop4"
+                ]
+            },
+            {
+                townName: "Weipa",
+                season: "Wet",
+                temperature: "17",
+                rainfall: "443",
+                ph: "8",
+                produce: [
+                    "Crop5",
+                    "Crop6"
+                ]
+            },
+            {
+                townName: "Lockhart River",
+                season: "Dry",
+                temperature: "15",
+                rainfall: "401",
+                ph: "7.2",
+                produce: "Crop7"
+            },
+            {
+                townName: "Aurukun",
+                season: "Wet",
+                temperature: "14",
+                rainfall: "398",
+                ph: "6.8",
+                produce: [
+                    "Crop1",
+                    "Crop3",
+                    "Crop5"
+                ]
+            },
+            {
+                townName: "Coen",
+                season: "Dry",
+                temperature: "18",
+                rainfall: "424",
+                ph: "5.9",
+                produce: [
+                    "Crop2",
+                    "Crop4",
+                    "Crop6"
+                ]
+            },
+            {
+                townName: "Pormpuraaw",
+                season: "Wet",
+                temperature: "15",
+                rainfall: "433",
+                ph: "7.1",
+                produce: [
+                    "Crop3",
+                    "Crop5",
+                    "Crop7"
+                ]
+            },
+            {
+                townName: "Kowanyama",
+                season: "Dry",
+                temperature: "16",
+                rainfall: "451",
+                ph: "6.4",
+                produce: [
+                    "Crop1",
+                    "Crop3",
+                    "Crop6"
+                ]
+            },
+            {
+                townName: "Cooktown",
+                season: "Wet",
+                temperature: "17",
+                rainfall: "456",
+                ph: "6.1",
+                produce: [
+                    "Crop2",
+                    "Crop4",
+                    "Crop7"
+                ]
+            },
+            {
+                townName: "Cairns",
+                season: "Dry",
+                temperature: "14",
+                rainfall: "444",
+                ph: "7.2",
+                produce: [
+                    "Crop1",
+                    "Crop2",
+                    "Crop4",
+                    "Crop7"
+                ]
+            },
+            {
+                townName: "Dimbulah",
+                season: "Wet",
+                temperature: "16",
+                rainfall: "419",
+                ph: "6.5",
+                produce: [
+                    "Crop2",
+                    "Crop3",
+                    "Crop6"
+                ]
+            },
+            {
+                townName: "Karumba",
+                season: "Dry",
+                temperature: "15",
+                rainfall: "412",
+                ph: "4.9",
+                produce: "Crop7"
+            },
+            {
+                townName: "Normanton",
+                season: "Wet",
+                temperature: "16",
+                rainfall: "407",
+                ph: "5.3",
+                produce: "Crop3"
+            }
+        ],
         customValues: []
     });
     
@@ -60,7 +203,7 @@ app.post('/form', (req, res) => {
         {
             res.redirect('/graph1')
         }
-        else if (req.body.ph > 7 && req.body.ph < 14)
+        else if (req.body.ph > 7 && req.body.ph <= 14)
         {
             res.redirect('/graph2')
         }
